367868d27ad6be6d567df5d855122327731d7c82
Duong Van Chan <duongchan159@gmail.com>
2022-06-20 10:39:15 +0700

    Initial commit

---
+++ FlickrImageViewer/Application/Main/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickrImageViewer
+//
+//  Created by LAP14121 on 15/06/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ FlickrImageViewer/Application/Main/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  FlickrImageViewer
+//
+//  Created by LAP14121 on 15/06/2022.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
