cc3f1387ced9744065edf07e5f118e712e34d5b7
Duong Van Chan <duongchan159@gmail.com>
2022-06-26 08:04:14 +0700

    feat: Add logout confirmation modal

--- FlickrImageViewer/Application/Home/UserProfile/UserProfileViewController.m
+++ FlickrImageViewer/Application/Home/UserProfile/UserProfileViewController.m
@@ -11,6 +11,8 @@
 
 @interface UserProfileViewController ()
 
+@property (nonatomic, strong) UIAlertController *logoutModal;
+
 @end
 
 @implementation UserProfileViewController
@@ -23,16 +25,41 @@
     UIBarButtonItem *logoutButton = [[UIBarButtonItem alloc] initWithTitle:@"Logout"
                                                                      style:UIBarButtonItemStylePlain
                                                                     target:self
-                                                                    action:@selector(logout)];
+                                                                    action:@selector(onLogoutButtonClicked)];
     [self.navigationItem setLeftBarButtonItem:logoutButton animated:YES];
 }
 
+#pragma mark - Handlers
+- (void)onLogoutButtonClicked {
+    [self presentViewController:self.logoutModal animated:YES completion:nil];
+}
 
-//#pragma mark - Private methods
+#pragma mark - Private methods
 - (void)logout {
     [LoginHandler.sharedLoginHandler removeUserAccessTokenAndSecret];
     /// Navigate to Login Screen
     [AppDelegate.shared.rootViewController switchToLogOut];
+}
+
+#pragma mark - Custom accessors
+- (UIAlertController *)logoutModal {
+    if (_logoutModal) return _logoutModal;
+    
+    _logoutModal = [UIAlertController alertControllerWithTitle:@"Hope to see you soon"
+                                                       message:@"Are you sure you want to logout?"
+                                                preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Log out"
+                                                     style:UIAlertActionStyleDestructive
+                                                   handler:^(UIAlertAction * _Nonnull action) {
+        [self logout];
+    }];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+    [_logoutModal addAction:action];
+    [_logoutModal addAction:cancelAction];
+    _logoutModal.preferredAction = action;
+    return _logoutModal;
 }
 
 @end
