215410aa626d6e1e44dbe6ee23351ca3689e51ac
Duong Van Chan <duongchan159@gmail.com>
2022-07-11 16:54:46 +0700

    feat: implement the upload selected photos to photo streams

---
+++ FlickrImageViewer/Application/Home/Upload/Networking/UploadNetworking.h
@@ -0,0 +1,16 @@
+//
+//  UploadNetworking.h
+//  FlickrImageViewer
+//
+//  Created by LAP14121 on 11/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UploadNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FlickrImageViewer/Application/Home/Upload/Networking/UploadNetworking.m
@@ -0,0 +1,12 @@
+//
+//  UploadNetworking.m
+//  FlickrImageViewer
+//
+//  Created by LAP14121 on 11/07/2022.
+//
+
+#import "UploadNetworking.h"
+
+@implementation UploadNetworking
+
+@end
